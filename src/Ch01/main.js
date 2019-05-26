636f21c2c66f3179179f98b0884d30ea2cdee326
Zone <zonetw.web@gmail.com>
2019-05-26 08:32:34 +0800

    124 remove function declaration: remove play in amountFor

--- src/Ch01/main.js
+++ src/Ch01/main.js
@@ -3,9 +3,9 @@ module.exports = function statement(invoice, plays) {
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
-    function amountFor(aPerformance, play) {
+    function amountFor(aPerformance) {
         let result = 0;
-        switch (play.type) {
+        switch (playFor(aPerformance).type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -20,7 +20,7 @@ module.exports = function statement(invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${play.type}`);
+                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
         }
 
         return result;
@@ -39,7 +39,7 @@ module.exports = function statement(invoice, plays) {
     for (let perf of invoice.performances) {
         let thisAmount = 0;
 
-        thisAmount += amountFor(perf, playFor(perf));
+        thisAmount += amountFor(perf);
 
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
