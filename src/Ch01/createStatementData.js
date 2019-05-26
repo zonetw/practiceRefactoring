851d966c0223865fa37c5807d5534f216007dbe5
Zone <zonetw.web@gmail.com>
2019-05-26 15:36:14 +0800

    272 Replace Conditional with Polymorphism: amount

--- src/Ch01/createStatementData.js
+++ src/Ch01/createStatementData.js
@@ -5,26 +5,7 @@ class PerformanceCalculator {
     }
 
     get amount() {
-        let result = 0;
-        switch (this.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${this.play.type}`);
-        }
-
-        return result;
+        throw new Error("subclass responsibility!");
     }
 
     get volumeCredits() {
@@ -48,10 +29,25 @@ function createPerformanceCalculator(aPerformance, aPlay) {
 }
 
 class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result;
+    }
 
 }
 
 class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
 
 }
 
@@ -83,4 +79,4 @@ module.exports = function createStatementData(invoice, plays) {
     statementData.totalAmount = totalAmount(statementData);
     statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return statementData;
-}
+};
