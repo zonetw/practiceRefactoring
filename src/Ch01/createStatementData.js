ca9d670680998198a0bcb35dcd314ac9da33a36b
Zone <zonetw.web@gmail.com>
2019-05-26 15:32:12 +0800

    272 Replace Conditional with Polymorphism: Calculator

--- src/Ch01/createStatementData.js
+++ src/Ch01/createStatementData.js
@@ -37,7 +37,22 @@ class PerformanceCalculator {
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-    return new PerformanceCalculator(aPerformance, aPlay);
+    switch (aPlay.type) {
+        case "tragedy":
+            return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy":
+            return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`unknow type: ${aPlay.type}`);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+
 }
 
 module.exports = function createStatementData(invoice, plays) {
