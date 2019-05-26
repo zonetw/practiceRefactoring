cd4f8519f179e56f0a0970f6a0e1596d38880ddb
Zone <zonetw.web@gmail.com>
2019-05-26 13:16:18 +0800

    prepare to store data ( play ) in another object

--- src/Ch01/main.js
+++ src/Ch01/main.js
@@ -1,7 +1,11 @@
 module.exports = function statement(invoice, plays) {
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        return result;
+    }
     const statementData = {};
     statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances;
+    statementData.performances = invoice.performances.map(enrichPerformance);
     return renderPlainText(statementData, plays);
 };
 
