f69d14831129fd78befdfbbfbe2fcb70427f1dfd
Zone <zonetw.web@gmail.com>
2019-05-26 15:10:15 +0800

    334 Replace constructor with factory function
    createPerformanceCalculator

--- src/Ch01/createStatementData.js
+++ src/Ch01/createStatementData.js
@@ -36,9 +36,13 @@ class PerformanceCalculator {
     }
 }
 
+function createPerformanceCalculator(aPerformance, aPlay) {
+    return new PerformanceCalculator(aPerformance, aPlay);
+}
+
 module.exports = function createStatementData(invoice, plays) {
     function enrichPerformance(aPerformance) {
-        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
         result.amount = calculator.amount;
